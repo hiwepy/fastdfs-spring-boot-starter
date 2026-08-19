005739b02a2e1a4fcc7ee5bf7abbd762a6b78f19
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-19 20:19:08 +0800

    fix: add comprehensive English Javadoc

--- src/main/java/com/github/tobato/fastdfs/spring/boot/FastdfsTemplate.java
+++ src/main/java/com/github/tobato/fastdfs/spring/boot/FastdfsTemplate.java
@@ -25,6 +25,7 @@ import com.github.tobato.fastdfs.spring.boot.utils.FastdfsUtils;
 /**
  * TODO
  * @author <a href="https://github.com/loong10k">Loong Wan</a>
+ * @since 1.0.0
  */
 public class FastdfsTemplate {
 
